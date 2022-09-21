c68fd26ab4a54cff3a461140c8f4f578c773017b
hanif2901 <hafizcurup@gmail.com>
2022-09-21 13:59:38 +0700

    update 1.1

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 // Storage Datas Input User
-const data1 = document.getElementById("data1").value;
-const data2 = document.getElementById("data2").value;
+const outputScreen1 = document.getElementById("output1");
+const outputScreen2 = document.getElementById("output2");
 
-let datas = {
-    varData1: [],
-    varData2: []
-};
+const datas = [
+    { varData1: "", varData2: "" }
+];
 
-let i = 0;
 function saveDatas() {
-    i++;
-    if (i <= 1) {
-        datas.varData1.push(data1);
-        datas.varData2.push(data2);
-        console.log("data di push..");
-        console.log(i);
+    const data1 = document.getElementById("data1").value;
+    const data2 = document.getElementById("data2").value;
+    if (!data1 && data2) {
+        alert('Data tak boleh kosong...')
     } else {
-        alert("Hanya boleh menginput 1 kali yah...");
+        datas[0].varData1 = data1
+        datas[0].varData2 = data2
+        outputScreen1.value = datas[0].varData1
+        outputScreen2.value = datas[0].varData2
+        return;
     }
 };
 // Storage Datas Input User END
-// Update Screen Output
-const outputScreen = document.getElementById("output");
-function showOutput() {
-    outputScreen.value = `Data 1 = ${datas.varData1}, Data 2 = ${datas.varData2}`
-}
-// Update Screen Output END
