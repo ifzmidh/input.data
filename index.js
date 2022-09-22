8368dca9bc814444df7d0371fbc4195997082f94
ifzmidh <hafizcurup@gmail.com>
2022-09-22 12:16:21 +0700

    update 1.2

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 const outputScreen1 = document.getElementById("output1");
 const outputScreen2 = document.getElementById("output2");
 
-const datas = [
-    { varData1: "", varData2: "" }
-];
+const datas = { 
+    varData1: "", 
+    varData2: "" 
+};
 
 function saveDatas() {
     const data1 = document.getElementById("data1").value;
@@ -12,11 +13,14 @@ function saveDatas() {
     if (!data1 && data2) {
         alert('Data tak boleh kosong...')
     } else {
-        datas[0].varData1 = data1
-        datas[0].varData2 = data2
-        outputScreen1.value = datas[0].varData1
-        outputScreen2.value = datas[0].varData2
+        datas.varData1 = data1
+        datas.varData2 = data2
+        outputScreen1.value = datas.varData1
+        outputScreen2.value = datas.varData2
         return;
     }
 };
 // Storage Datas Input User END
+
+const parseData1 = JSON.parse(datas).varData1
+console.log(parseData1)
